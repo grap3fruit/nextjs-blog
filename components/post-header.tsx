f15b24cc82f20eec2c9eb348e75a4348af8fb195
grap3fruit <swkwon333@gmail.com>
2021-01-16 17:08:06 +0900

    [feat] post 관련 css 조절

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -15,17 +15,23 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
+      <div className="hidden md:flex md:mb-12">
+        <div className="">
+          <Avatar name={author.name} picture={author.picture} />
+        </div>
+        <p className="md:mt-3 ml-3 md text-lg">·</p>
+        <div className="md:mt-3 ml-3 md text-lg">
+          <DateFormatter dateString={date} />
+        </div>
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
+      <div className="max-w-4xl mb-8 md:mb-16 mx-auto">
         <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
         </div>
-        <div className="mb-6 text-lg">
+        <div className="block md:hidden mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
       </div>
