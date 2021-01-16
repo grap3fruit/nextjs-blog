a347a1d5b8ca8d63fa92ff707a990ba573b86694
grap3fruit <swkwon333@gmail.com>
2021-01-16 19:27:46 +0900

    [feat] utterances 작업 완료

--- components/uttarences.tsx
+++ components/uttarences.tsx
@@ -25,25 +25,22 @@ import React, { Component, useEffect } from 'react';
 // export default Utterances;
 
 const Utterances: React.FC = () => (
-  <>
-    <p>123</p>
-    <section
-      ref={(elem) => {
-        if (!elem) {
-          return;
-        }
-        const scriptElem = document.createElement('script');
-        scriptElem.src = 'https://utteranc.es/client.js';
-        scriptElem.async = true;
-        scriptElem.crossOrigin = 'anonymous';
-        scriptElem.setAttribute('repo', 'grap3fruit/nextjs-blog');
-        scriptElem.setAttribute('issue-term', 'pathname');
-        scriptElem.setAttribute('label', 'blog-comment');
-        scriptElem.setAttribute('theme', 'github-light');
-        elem.appendChild(scriptElem);
-      }}
-    />
-  </>
+  <section
+    ref={(elem) => {
+      if (!elem) {
+        return;
+      }
+      const scriptElem = document.createElement('script');
+      scriptElem.src = 'https://utteranc.es/client.js';
+      scriptElem.async = true;
+      scriptElem.crossOrigin = 'anonymous';
+      scriptElem.setAttribute('repo', 'grap3fruit/nextjs-blog');
+      scriptElem.setAttribute('issue-term', 'title');
+      scriptElem.setAttribute('label', 'blog-comment');
+      scriptElem.setAttribute('theme', 'github-light');
+      elem.appendChild(scriptElem);
+    }}
+  />
 );
 
 export default Utterances;
