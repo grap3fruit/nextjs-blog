f06e93c7d72401c890071a6d19d638824297d45a
grap3fruit <swkwon333@gmail.com>
2021-01-16 19:28:59 +0900

    [feat] favicon 추가 / 전체 css 수정

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -7,11 +7,12 @@ type Props = {
 
 const MoreStories = ({ posts }: Props) => {
   return (
-    <section>
+    <section className="flex justify-between">
       {/* <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight"></h2> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-        {/* <div className="grid grid-cols-1 gap-x-16 gap-y-20 mb-32"> */}
-        {/* <div className="md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32"> */}
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32"> */}
+      {/* <div className="grid grid-cols-1 gap-x-16 gap-y-20 mb-32"> */}
+      <div></div>
+      <div className="mb-32 max-w-5xl">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
@@ -24,6 +25,7 @@ const MoreStories = ({ posts }: Props) => {
           />
         ))}
       </div>
+      <div></div>
     </section>
   );
 };
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -3,6 +3,7 @@ import DateFormatter from './date-formatter';
 import CoverImage from './cover-image';
 import Link from 'next/link';
 import Author from '../types/author';
+import cn from 'classnames';
 
 type Props = {
   title: string;
@@ -15,22 +16,32 @@ type Props = {
 
 const PostPreview = ({ title, coverImage, date, excerpt, author, slug }: Props) => {
   return (
-    <div>
-      <div className="mb-5">
+    // <div className="flex mb-10">
+    <div
+      className={cn(
+        {
+          'hover:shadow-medium transition-shadow duration-200': slug,
+        },
+        'flex p-5'
+      )}
+    >
+      <div className="mb-5 w-1/3">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <div className="ml-10 w-2/3">
+        <h3 className="text-2xl mb-2 leading-snug">
+          <Link as={`/posts/${slug}`} href="/posts/[slug]">
+            <p className="cursor-pointer">{title}</p>
+          </Link>
+        </h3>
+        <div className="text-base mb-2">
+          <DateFormatter dateString={date} />
+        </div>
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+          <a className="text-base leading-relaxed mb-4">{excerpt}</a>
         </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
+        {/* <Avatar name={author.name} picture={author.picture} /> */}
       </div>
-      <Link as={`/posts/${slug}`} href="/posts/[slug]">
-        <a className="text-lg leading-relaxed mb-4">{excerpt}</a>
-      </Link>
-      {/* <Avatar name={author.name} picture={author.picture} /> */}
     </div>
   );
 };
