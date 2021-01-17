23ae7012991cf4fbe504e9edf589ef16c4d40b85
grap3fruit <swkwon333@gmail.com>
2021-01-17 22:03:13 +0900

    [feat] preview css 수정
    작은 화면에서 css 이상해지는것 변경

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -17,32 +17,33 @@ type Props = {
 const PostPreview = ({ title, coverImage, date, excerpt, author, slug }: Props) => {
   return (
     // <div className="flex mb-10">
-    <div
-      className={cn(
-        {
-          'hover:shadow-medium transition-shadow duration-200': slug,
-        },
-        'flex p-5'
-      )}
-    >
-      <div className="mb-5 w-1/3">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <div className="ml-10 w-2/3">
-        <h3 className="text-2xl mb-2 leading-snug">
-          <Link as={`/posts/${slug}`} href="/posts/[slug]">
-            <p className="cursor-pointer">{title}</p>
-          </Link>
-        </h3>
-        <div className="text-base mb-2">
-          <DateFormatter dateString={date} />
+    <Link as={`/posts/${slug}`} href="/posts/[slug]">
+      <div
+        className={cn(
+          {
+            'hover:shadow-medium transition-shadow duration-200': slug,
+          },
+          'flex p-5 cursor-pointer'
+        )}
+      >
+        <div className="mb-5 hidden md:block md:w-1/3">
+          <CoverImage slug={slug} title={title} src={coverImage} />
         </div>
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="text-base leading-relaxed mb-4">{excerpt}</a>
-        </Link>
-        {/* <Avatar name={author.name} picture={author.picture} /> */}
+        <div className="md:ml-10 md:w-2/3">
+          <div className="mb-5 block md:hidden">
+            <CoverImage slug={slug} title={title} src={coverImage} />
+          </div>
+          <h3 className="text-2xl mb-2 leading-snug">
+            <p>{title}</p>
+          </h3>
+          <div className="text-base mb-2">
+            <DateFormatter dateString={date} />
+          </div>
+          <p className="text-base leading-relaxed mb-4">{excerpt}</p>
+          {/* <Avatar name={author.name} picture={author.picture} /> */}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
