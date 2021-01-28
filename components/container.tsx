fd36b9f1f635e748617f71b6f63a712a7adbff7b
grap3fruit <swkwon333@gmail.com>
2021-01-28 14:55:14 +0900

    [feat] container max-width 조절. lg 이하에서 3xl로 변경

--- components/container.tsx
+++ components/container.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const Container: FunctionComponent = ({ children }: Props) => {
-  return <div className="container max-w-4xl mx-auto px-5">{children}</div>;
+  return <div className="container max-w-3xl lg:max-w-4xl mx-auto px-5">{children}</div>;
 };
 
 export default Container;
