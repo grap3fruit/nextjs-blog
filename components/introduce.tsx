38095d05cdc070984339c2b07a83fa4e124f0cb7
grap3fruit <swkwon333@gmail.com>
2021-01-30 23:44:51 +0900

    [feat] indtroduce 내용 수정

--- components/introduce.tsx
+++ components/introduce.tsx
@@ -7,7 +7,7 @@ const Introduce = () => {
       />
       <div className="mb-5 text-2xl">안녕하세요! grap3fruit의 블로그 입니다.</div>
       <div className="mb-5 text-base">
-        현재 Web FE 직무를 준비하고 있으며, Web FE, BE, UI/UX 그리고 Machine Learning에 관심이
+        현재 Web FE 직무를 준비하고 있으며, Web FE, BE, UX 그리고 Machine Learning에 관심이
         있습니다.😊{' '}
       </div>
     </section>
