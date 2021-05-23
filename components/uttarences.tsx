a9f5186d79f79bff3eecdf127f3eea2f7fe14d97
grap3fruit <swkwon333@gmail.com>
2021-05-23 14:06:18 +0900

    [fix] Fix fbclid bug
    페이스북 링크로 연결되었을때, url이 query string과 함께 리렌더링 되어서
    utterances가 2번 호출되어 2개 생성되던 버그 수정
    애초에 리렌더링 되는걸 막아야 하는데, 일단 임시로 해결

--- components/uttarences.tsx
+++ components/uttarences.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-const Utterances: React.FC = () => (
-  <section
-    ref={(elem) => {
-      if (!elem) {
-        return;
-      }
-      const scriptElem = document.createElement('script');
-      scriptElem.src = 'https://utteranc.es/client.js';
-      scriptElem.async = true;
-      scriptElem.crossOrigin = 'anonymous';
-      scriptElem.setAttribute('repo', 'grap3fruit/nextjs-blog');
-      scriptElem.setAttribute('issue-term', 'title');
-      scriptElem.setAttribute('label', 'blog-comment');
-      scriptElem.setAttribute('theme', 'github-light');
-      elem.appendChild(scriptElem);
-    }}
-  />
-);
+const Utterances: React.FC = () => {
+  const [isAlreadyCalled, setIsAlreadyCalled] = useState(false);
+  const router = useRouter();
+
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem) {
+          return;
+        }
+        const scriptElem = document.createElement('script');
+        scriptElem.src = 'https://utteranc.es/client.js';
+        scriptElem.async = true;
+        scriptElem.crossOrigin = 'anonymous';
+        scriptElem.setAttribute('repo', 'grap3fruit/nextjs-blog');
+        scriptElem.setAttribute('issue-term', 'title');
+        scriptElem.setAttribute('label', 'blog-comment');
+        scriptElem.setAttribute('theme', 'github-light');
+        if (!isAlreadyCalled) {
+          elem.appendChild(scriptElem);
+          setIsAlreadyCalled(true);
+        }
+      }}
+    />
+  );
+};
 
 export default Utterances;
